1c37fc645ce3514a7011e507344e76708dc47f3c
rajeebkm <rkm2541997@gmail.com>
2024-12-09 12:29:58 +0530

    changeAdmin script added

---
+++ executions/03-changeAdmin.ts
@@ -0,0 +1,37 @@
+import { ethers, network } from "hardhat"
+import * as helpers from "@nomicfoundation/hardhat-network-helpers"
+import { FlashDuels, FLASHUSDC } from "../typechain-types"
+import FlashDuelsABI from "../constants/abis/FlashDuels.json"
+import FLASHUSDCABI from "../constants/abis/FLASHUSDC.json"
+import netMap from "../constants/networkMapping.json"
+import { forkedChain, networkConfig } from "../helper-hardhat-config"
+import OwnershipABI from "../constants/abis/OwnershipFacet.json"
+
+const main = async () => {
+    let tx, txr, deployer, sequencer, liquidator, rajeeb
+    const networkName: any = network.name as keyof typeof netMap
+
+    if (forkedChain.includes(networkName)) {
+        console.log("here")
+        await helpers.mine()
+        const provider = ethers.provider
+        deployer = new ethers.Wallet(process.env.PRIVATE_KEY_ADMIN!.toString(), provider)
+    } else {
+        ;[deployer, , sequencer, liquidator] = await ethers.getSigners()
+    }
+
+    // console.log(deployer)
+
+    const flashUSDC = new ethers.Contract(netMap[networkName].FLASHUSDC, FLASHUSDCABI, deployer)
+
+    tx = await flashUSDC.changeAdmin("0x2Eb671E6e0cd965A79A80caF35c5123b7a5D8ebb");
+    await tx.wait(1);
+    
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error: any) => {
+        console.error(error)
+        process.exit(1)
+    })
