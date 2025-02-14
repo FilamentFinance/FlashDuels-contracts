8658832f515ebd8943e4450261b3d02b2549ffef
rajeebkm <rkm2541997@gmail.com>
2025-02-14 18:18:41 +0530

    scripts added

--- executions/04-check-duels.ts
+++ executions/04-check-duels.ts
@@ -20,13 +20,13 @@ const main = async () => {
     }
 
     const flashDuelsView: FlashDuels = new ethers.Contract(netMap[networkName].Diamond, FlashDuelsViewABI, deployer)
-    const flashUSDC: FLASHUSDC = new ethers.Contract(netMap[networkName].FLASHUSDC, FLASHUSDCABI, deployer)
+    // const flashUSDC: FLASHUSDC = new ethers.Contract(netMap[networkName].FLASHUSDC, FLASHUSDCABI, deployer)
 
-    tx = await flashDuelsView.checkIfThresholdMet("f414a841f8e14c39aa7cc9d18cb9667f07c2d13dda67962536b32dfd19d343cc")
+    tx = await flashDuelsView.checkIfThresholdMet("7fe60b106113aa3c8b95b22462b1fd0089c91f367fcbec58e105d19e907946e1")
     console.log("IsThresholdMet", tx)
-    tx = await flashDuelsView.checkIfThresholdMet("f414a841f8e14c39aa7cc9d18cb9667f07c2d13dda67962536b32dfd19d343cc")
+    tx = await flashDuelsView.checkIfThresholdMet("7fe60b106113aa3c8b95b22462b1fd0089c91f367fcbec58e105d19e907946e1")
     console.log("IsThresholdMet", tx)
-    tx = await flashDuelsView.getCryptoDuel("f414a841f8e14c39aa7cc9d18cb9667f07c2d13dda67962536b32dfd19d343cc")
+    tx = await flashDuelsView.getCryptoDuel("9b512e34fdbf3950665edfbe6e689158b41bdaa3dcdd0b419d11ea4c474fd2fc")
     console.log("Get CryptoDuel", tx)
 
 
@@ -39,3 +39,19 @@ main()
         process.exit(1)
     })
 
+
+    // old
+        // "seiTestnet": {
+        //     "FLASHUSDC": "0x542938B5c37d082159C0933EC982E3585c94BD62",
+        //     "FlashDuelsAdminFacet": "0x88498614Af73Ba8643C8190DBeDE42eff14EF102",
+        //     "FlashDuelsCoreFacet": "0x9c34E4e6c6075A35a23d13BF2d50ed5b1Af787a3",
+        //     "FlashDuelsMarketplaceFacet": "0x9faA711B06597BD3a757cFD716c52E72f035E11D",
+        //     "FlashDuelsViewFacet": "0xc4A1AE1234e39987c79aE14E00f2A98306C35236",
+        //     "OwnershipFacet": "0x65c36658c81EAed25f207C91a8A3F1e82E36ABd1",
+        //     "DiamondCutFacet": "0x54eB40902D7cb919EF28AbE286050Ecee2cB9b2B",
+        //     "DiamondLoupeFacet": "0x86AE9F81809D3cC09e80433c6E41F7fd98eD09AF",
+        //     "DiamondInit": "0x639FebBc3b0DF17e10b65E7B69138265A3e4648A",
+        //     "Diamond": "0x82f8b57891C7EC3c93ABE194dB80e4d8FC931F09",
+        //     "StartBlock": 150967382,
+        //     "Goldsky_Subgraph": ""
+  
