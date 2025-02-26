a088e29f198502224526e31ee6edb9269ec6f5a8
rajeebkm <rkm2541997@gmail.com>
2025-02-26 23:27:30 +0530

    script updated

--- executions/07-optionTokenTalance.ts
+++ executions/07-optionTokenTalance.ts
@@ -19,11 +19,19 @@ const main = async () => {
     } else {
         ;[deployer, , sequencer, liquidator] = await ethers.getSigners()
     }
-
     const flashDuelsView: FlashDuels = new ethers.Contract(netMap[networkName].Diamond, FlashDuelsViewABI, deployer)
+    // tx = await flashDuelsView.getDuelIdToOptions("76960d2ea0df8d5f885ffc91306a149e7620462f0322d2e9af9e630282ef623f")
+    // console.log(tx)
+    tx = await flashDuelsView.getOptionIndexToOptionToken("76960d2ea0df8d5f885ffc91306a149e7620462f0322d2e9af9e630282ef623f", 0)
+    console.log(tx)
+    const optionToken: OptionToken = new ethers.Contract("0xc9f2501E9bae67C52123263ad5CA526cfC0770Af", OptionTokenABI, deployer);
+
+    tx = await optionToken.allowance("0x8F5D833272017fF60E4A64121779E006B8E598a9", "0xF513eaD7B64D9D9aF17122C10f9e7e3a106e6121");
+    console.log(tx)
+
     
-    tx = await flashDuelsView.getAllTimeEarnings("0x2Eb671E6e0cd965A79A80caF35c5123b7a5D8ebb")
-    console.log("optionToken balance", tx)
+    // tx = await flashDuelsView.getAllTimeEarnings("0x2Eb671E6e0cd965A79A80caF35c5123b7a5D8ebb")
+    // console.log("optionToken balance", tx)
 
 }
 
