af82bde6db65da263fa915c9bdba6a956453623a
rajeebkm <rkm2541997@gmail.com>
2025-04-24 14:16:41 +0530

    faucet transfer script added

---
+++ executions/13-faucetTransfer.ts
@@ -0,0 +1,53 @@
+import { ethers, network } from "hardhat"
+import netMap from "../constants/networkMapping.json"
+import { Credits } from "../typechain-types"
+import { forkedChain } from "../helper-hardhat-config"
+import * as helpers from "@nomicfoundation/hardhat-network-helpers"
+import FlashDuelsAdminFacetABI from "../constants/abis/FlashDuelsAdminFacet.json"
+import FlashDuelsViewFacetABI from "../constants/abis/FlashDuelsViewFacet.json"
+import { ParticipationTokenType } from "../helper-hardhat-config"
+import CreditsABI from "../constants/abis/Credits.json"
+
+const main = async () => {
+    let tx, deployer, sequencer, liquidator
+    const networkName: any = network.name as keyof typeof netMap
+    const chainName = networkName as keyof typeof netMap
+
+    if (forkedChain.includes(networkName)) {
+        console.log("here")
+        await helpers.mine()
+        const provider = ethers.provider
+        deployer = new ethers.Wallet(process.env.PRIVATE_KEY_ADMIN!.toString(), provider)
+    } else {
+        ;[deployer, , sequencer, liquidator] = await ethers.getSigners()
+    }
+
+    // const usdcContract: FLASHUSDC = new ethers.Contract(netMap[chainName].FLASHUSDC, FLASHUSDCABI, usdcAdmin)
+    const usdcContract: Credits = new ethers.Contract(netMap[chainName].FlashDuelsCredits, CreditsABI, deployer)
+    console.log(await usdcContract.balanceOf("0x23bC3e7F43Fd7bD10aa27534ff11E871ac4B7C8E"))
+
+
+    let amount = ethers.parseUnits("10000", 18)
+    tx = await usdcContract.connect(deployer).airdrop([deployer.address], [amount])
+    await tx.wait(1)
+
+    tx = await usdcContract.connect(deployer).claim()
+    await tx.wait(1)
+
+    console.log(await usdcContract.balanceOf(deployer.address))
+
+
+    tx = await usdcContract.connect(deployer).transfer("0x23bC3e7F43Fd7bD10aa27534ff11E871ac4B7C8E", amount)
+    await tx.wait(1)
+
+    console.log(await usdcContract.balanceOf("0x23bC3e7F43Fd7bD10aa27534ff11E871ac4B7C8E"))
+
+
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error: any) => {
+        console.error(error)
+        process.exit(1)
+    })
